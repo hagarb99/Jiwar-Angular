f8dc7f48433fa10baa8944361b2f3aad5d37de4e
Hagar Bastawi <jp.hagarbastawi@gmail.com>
2026-01-20 18:46:56 +0200

    Edit url

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiBaseUrl: 'https://jiwar.runasp.net/api',
+    apiBaseUrl: 'http://jiwar.runasp.net/api',
     assetsBaseUrl: 'http://jiwar.runasp.net'
 };
